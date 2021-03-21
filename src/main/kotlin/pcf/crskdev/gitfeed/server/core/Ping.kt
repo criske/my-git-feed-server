0260efacd27dd238dbfc11d7a7abb768f172ed66
cristianpela <cristypela05@gmail.com>
2021-03-21 16:15:39 +0200

    ping update.

--- src/main/kotlin/pcf/crskdev/gitfeed/server/core/Ping.kt
+++ src/main/kotlin/pcf/crskdev/gitfeed/server/core/Ping.kt
@@ -25,4 +25,4 @@
 
 package pcf.crskdev.gitfeed.server.core
 
-data class Ping(val ping: String = "My-Git-Feed API is up and running")
+data class Ping(val ping: String = "My-Git-Feed API is up and running!")
