a9ccfbfa6f966b487be4300e6cf9fbb9c19597e8
cristianpela <cristypela05@gmail.com>
2021-04-28 13:20:45 +0300

    Bitbucket me# implemented and tested.

--- src/main/kotlin/pcf/crskdev/gitfeed/server/core/feed/bitbucket/BitbucketGitFeed.kt
+++ src/main/kotlin/pcf/crskdev/gitfeed/server/core/feed/bitbucket/BitbucketGitFeed.kt
@@ -24,12 +24,17 @@
  */
 package pcf.crskdev.gitfeed.server.core.feed.bitbucket
 
+import com.fasterxml.jackson.databind.JsonNode
 import pcf.crskdev.gitfeed.server.core.feed.GitFeed
 import pcf.crskdev.gitfeed.server.core.feed.models.Assignments
 import pcf.crskdev.gitfeed.server.core.feed.models.Commits
 import pcf.crskdev.gitfeed.server.core.feed.models.Repos
 import pcf.crskdev.gitfeed.server.core.feed.models.User
 import pcf.crskdev.gitfeed.server.core.net.RequestClient
+import pcf.crskdev.gitfeed.server.core.net.request
+import pcf.crskdev.gitfeed.server.core.util.ObjectScope
+import pcf.crskdev.gitfeed.server.core.util.obj
+import java.net.URI
 
 /**
  * Bitbucket git feed implementation.
@@ -52,11 +57,43 @@ class BitbucketGitFeed(private val client: RequestClient) : GitFeed {
         TODO("Not yet implemented")
     }
 
-    override fun me(): User {
-        TODO("Not yet implemented")
-    }
+    override fun me(): User = this.client
+        .request(URI.create("$baseUrl/user")) { user(it.body) }
 
     override fun repos(page: Int?): Repos {
         TODO("Not yet implemented")
     }
+
+    /**
+     * Extract user from a bitbucket json response key.
+     *
+     * @return JsonNode
+     */
+    private fun ObjectScope.user(node: JsonNode) = obj {
+        applyUser(this, node)
+    }
+
+    /**
+     * Extract user from a bitbucket json response key.
+     *
+     * @return JsonNode
+     */
+    private fun user(node: JsonNode) = obj {
+        applyUser(this, node)
+    }.asTree()
+
+    /**
+     * Apply extracted user data from a bitbucket json response key to ObjectScope.
+     *
+     * @param objectScope ObjectScope.
+     * @param node JsonNode.
+     */
+    private fun applyUser(objectScope: ObjectScope, node: JsonNode) =
+        objectScope.apply {
+            "name" to node["username"]
+            "avatar" to node["links"]["avatar"]["href"]
+            "url" to node["links"]["html"]["href"]
+            "type" to node["type"]
+            "provider" to "Bitbucket"
+        }
 }
